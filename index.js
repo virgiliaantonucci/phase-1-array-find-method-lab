e8c5f2738eb67a3da91edfb9dbfef25d661810a0
Virgilia Antonucci <virgilia.antonucci@gmail.com>
2022-05-20 16:05:31 -0400

    commit tree 116c7ae3ff9c593b989149544a5895c9e88ad3a4

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+function superbowlWin(array){
+    let date = array.find(x => x.result === "W")
+        if (date) {
+            return date.year
+        }
+        else {
+            return undefined
+        }
+}
